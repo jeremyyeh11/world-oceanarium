c4b0cb0f4cd202dc64ba75da8a3f96ecd782e4e1
jeremyyeh11 <jeremyyeh11@users.noreply.github.com>
2026-06-05 07:25:29 +0000

    Uppercase atlas common name

--- src/version.js
+++ src/version.js
@@ -1,3 +1,3 @@
-export const APP_VERSION = 'v0.8.6-dev_27'
+export const APP_VERSION = 'v0.8.6-dev_28'
 export const APP_VERSION_LABEL = `world oceanarium ${APP_VERSION}`
 export const APP_VERSION_SHORT_LABEL = APP_VERSION
